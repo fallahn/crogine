70731fc6ed088f40fe978441ef9c306e4b801da6
fallahn <matty_styles@hotmail.com>
2017-04-07 15:07:39 +0100

    add model format converter

---
+++ ModelConverter/MainWindow.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace ModelConverter
+{
+    partial class ModelConverter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxPath = new System.Windows.Forms.TextBox();
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.groupBoxInfo = new System.Windows.Forms.GroupBox();
+            this.labelInfo = new System.Windows.Forms.Label();
+            this.groupBoxInfo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxPath
+            // 
+            this.textBoxPath.Location = new System.Drawing.Point(13, 13);
+            this.textBoxPath.Name = "textBoxPath";
+            this.textBoxPath.ReadOnly = true;
+            this.textBoxPath.Size = new System.Drawing.Size(292, 20);
+            this.textBoxPath.TabIndex = 0;
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Location = new System.Drawing.Point(312, 13);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(75, 23);
+            this.buttonOpen.TabIndex = 1;
+            this.buttonOpen.Text = "Open";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(312, 244);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.TabIndex = 2;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // groupBoxInfo
+            // 
+            this.groupBoxInfo.Controls.Add(this.labelInfo);
+            this.groupBoxInfo.Location = new System.Drawing.Point(13, 39);
+            this.groupBoxInfo.Name = "groupBoxInfo";
+            this.groupBoxInfo.Size = new System.Drawing.Size(374, 199);
+            this.groupBoxInfo.TabIndex = 3;
+            this.groupBoxInfo.TabStop = false;
+            this.groupBoxInfo.Text = "Info";
+            // 
+            // labelInfo
+            // 
+            this.labelInfo.AutoSize = true;
+            this.labelInfo.Location = new System.Drawing.Point(19, 27);
+            this.labelInfo.Name = "labelInfo";
+            this.labelInfo.Size = new System.Drawing.Size(81, 13);
+            this.labelInfo.TabIndex = 0;
+            this.labelInfo.Text = "No File Opened";
+            // 
+            // ModelConverter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(398, 274);
+            this.Controls.Add(this.groupBoxInfo);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonOpen);
+            this.Controls.Add(this.textBoxPath);
+            this.Name = "ModelConverter";
+            this.Text = "Model Converter";
+            this.groupBoxInfo.ResumeLayout(false);
+            this.groupBoxInfo.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxPath;
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.GroupBox groupBoxInfo;
+        private System.Windows.Forms.Label labelInfo;
+    }
+}
+
---
+++ ModelConverter/MainWindow.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.IO;
+using Assimp.Configs;
+
+namespace ModelConverter
+{
+    public partial class ModelConverter : Form
+    {
+        //ctor
+        public ModelConverter()
+        {
+            InitializeComponent();
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+
+
+        //callbacks
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog od = new OpenFileDialog();
+            od.Filter = "Obj Files|*.obj";
+            if(od.ShowDialog() == DialogResult.OK && loadFile(od.FileName))
+            {
+                textBoxPath.Text = od.FileName;
+            }
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sd = new SaveFileDialog();
+            sd.Filter = "Cro Model File|*.cmf";
+            if (sd.ShowDialog() == DialogResult.OK)
+            {
+                if (File.Exists(sd.FileName))
+                {
+                    //warn overwriting
+                    if (MessageBox.Show("File Exists. Overwrite?", "Warning", MessageBoxButtons.OKCancel) == DialogResult.Cancel)
+                    {
+                        return;
+                    }
+                }
+                outputFile(sd.FileName);
+            }
+            //outputFile("");
+        }
+
+
+        //funcs
+        private Assimp.Scene m_scene;
+        private bool loadFile(string path)
+        {
+            Assimp.AssimpContext importer = new Assimp.AssimpContext();
+            importer.SetConfig(new NormalSmoothingAngleConfig(66f));
+            //TODO check which other post processes we need
+            m_scene = importer.ImportFile(path, Assimp.PostProcessSteps.CalculateTangentSpace);
+
+            //failed loading :(
+            if (!m_scene.HasMeshes)
+            {
+                showGroupBoxMessage("No Valid Meshes found.");
+                return false;
+            }
+
+            //display some info
+            string msg = "Mesh Count: " + m_scene.MeshCount.ToString();
+
+            showGroupBoxMessage(msg);
+
+            return true;
+        }
+
+        private void outputFile(string name)
+        {
+            labelInfo.Text = "Processing...";
+
+            if (m_scene.MeshCount == 1)
+            {
+                //write with normal name
+                processOutput(name, m_scene.Meshes[0]);
+            }
+            else
+            {
+                //append each file with idx
+                for(var i = 0; i < m_scene.MeshCount; ++i)
+                {
+                    string output = name.Replace(".cmf", i.ToString() + ".cmf");
+                    processOutput(output, m_scene.Meshes[i]);
+                }
+            }
+
+            labelInfo.Text += "\n\nDone!";
+        }
+
+        private void processOutput(string output, Assimp.Mesh mesh)
+        {
+            const int Position = 0;
+            const int Colour = 1;
+            const int Normal = 2;
+            const int Tangent = 3;
+            const int Bitangent = 4;
+            const int UV0 = 5;
+            const int UV1 = 6;
+            byte flags = (1 << Position) | (1 << Normal);
+
+            //append any messages to info label
+            if (!mesh.HasVertices)
+            {
+                labelInfo.Text += "\nNo vertex positions found. File not written.";
+                return;
+            }
+            if(mesh.HasVertexColors(0))
+            {
+                flags |= (1 << Colour);
+            }
+
+            if (!mesh.HasNormals)
+            {
+                labelInfo.Text += "\nNo normals found. File not written.";
+                return;
+            }
+            if(!mesh.HasTangentBasis)
+            {
+                labelInfo.Text += "\nMesh tangents were missing...";
+            }
+            else
+            {
+                flags |= (1 << Tangent) | (1 << Bitangent);
+            }
+
+            if (!mesh.HasTextureCoords(0))
+            {
+                labelInfo.Text += "\nPrimary texture coords are missing, textures will appear undefined.";
+            }
+            else
+            {
+                flags |= (1 << UV0);
+            }
+            if (!mesh.HasTextureCoords(1))
+            {
+                labelInfo.Text += "\nSecondary texture coords are missing,\n\tlightmapping will be unavailable for this mesh.";
+            }
+            else
+            {
+                flags |= (1 << UV1);
+            }
+            //labelInfo.Text += "\n" + flags.ToString();
+
+            // cmf FILE FORMAT
+            /* HEADER
+             * flags byte
+             * byte index array count (max 32, default 1)
+             * <index count> int values representing index array offsets
+             * <index count> int values holding index array sizes
+             * -------------------------------------------------
+             * VBO data interleaved as flags, starts at HEADER size
+             * IBO data starts at IBOOFFSET[0] for IBOSIZE[0]
+             * All index buffer values are uint32
+             * */
+
+            //build VBO data
+            List<float> vboData = new List<float>();
+            for(var i = 0; i < mesh.VertexCount; ++i)
+            {
+                var pos = mesh.Vertices[i];
+                vboData.Add(pos.X);
+                vboData.Add(pos.Y);
+                vboData.Add(pos.Z);
+
+                if((flags & (1 << Colour)) != 0)
+                {
+                    var colour = mesh.VertexColorChannels[0][i];
+                    vboData.Add(colour.R);
+                    vboData.Add(colour.G);
+                    vboData.Add(colour.B);
+                }
+
+                var normal = mesh.Normals[i];
+                vboData.Add(normal.X);
+                vboData.Add(normal.Y);
+                vboData.Add(normal.Z);
+
+                if((flags & (1 << Tangent))  != 0)
+                {
+                    var tan = mesh.Tangents[i];
+                    var bitan = mesh.BiTangents[i];
+                    vboData.Add(tan.X);
+                    vboData.Add(tan.Y);
+                    vboData.Add(tan.Z);
+
+                    vboData.Add(bitan.X);
+                    vboData.Add(bitan.Y);
+                    vboData.Add(bitan.Z);
+                }
+
+                if((flags & (1 << UV0)) != 0)
+                {
+                    var uv = mesh.TextureCoordinateChannels[0][i];
+                    vboData.Add(uv.X);
+                    vboData.Add(uv.Y);
+                }
+
+                if((flags & (1 << UV1)) != 0)
+                {
+                    var uv = mesh.TextureCoordinateChannels[1][i];
+                    vboData.Add(uv.X);
+                    vboData.Add(uv.Y);
+                }
+            }
+
+            var indexArray = mesh.GetUnsignedIndices();
+
+            byte arrayCount = 1;
+            int arrayOffset = (sizeof(int) * 2) + 2; //(array offset + array size) + array count + flags
+            arrayOffset += (vboData.Count * sizeof(float));
+            int arraySize = indexArray.Length * sizeof(uint);
+
+            using (BinaryWriter writer = new BinaryWriter(File.Open(output, FileMode.Create)))
+            {
+                writer.Write(flags);
+                writer.Write(arrayCount);
+                writer.Write(arrayOffset);
+                writer.Write(arraySize);
+                foreach(var v in vboData)
+                {
+                    writer.Write(v);
+                }
+                foreach(var i in indexArray)
+                {
+                    writer.Write(i);
+                }
+            }
+        }
+
+        private void showGroupBoxMessage(string msg)
+        {
+            //groupBoxInfo.Content.
+            //Label l = new Label();
+            //l.Text = msg;
+            //groupBoxInfo.Controls.Add(l);
+            labelInfo.Text = msg;
+        }
+    }
+}
