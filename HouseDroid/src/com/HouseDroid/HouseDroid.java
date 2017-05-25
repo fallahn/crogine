a78d9224f9a2e40b08e808f05e066802027eac1e
fallahn <matty_styles@hotmail.com>
2017-05-25 21:28:45 +0100

    add android projects for the house thing

---
+++ HouseDroid/src/com/HouseDroid/HouseDroid.java
@@ -0,0 +1,15 @@
+
+package com.HouseDroid;
+
+import android.os.Bundle;
+import org.libsdl.app.SDLActivity;
+
+public class HouseDroid extends SDLActivity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+}
