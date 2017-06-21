1cff1a01c59e659c0ba984eab5453612bf54dbb5
fallahn <matty_styles@hotmail.com>
2017-06-21 16:33:44 +0100

    add empty class for updating player weapons
    Former-commit-id: 2c0673e11bea888c058b37f4227fa9237e520691

---
+++ testwin/src/PlayerWeaponsSystem.hpp
@@ -0,0 +1,70 @@
+/*-----------------------------------------------------------------------
+
+Matt Marchant 2017
+http://trederia.blogspot.com
+
+crogine test application - Zlib license.
+
+This software is provided 'as-is', without any express or
+implied warranty.In no event will the authors be held
+liable for any damages arising from the use of this software.
+
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute
+it freely, subject to the following restrictions :
+
+1. The origin of this software must not be misrepresented;
+you must not claim that you wrote the original software.
+If you use this software in a product, an acknowledgment
+in the product documentation would be appreciated but
+is not required.
+
+2. Altered source versions must be plainly marked as such,
+and must not be misrepresented as being the original software.
+
+3. This notice may not be removed or altered from any
+source distribution.
+
+-----------------------------------------------------------------------*/
+
+#ifndef TL_PLAYER_WEAPONS_HPP_
+#define TL_PLAYER_WEAPONS_HPP_
+
+#include <crogine/ecs/System.hpp>
+
+#include <vector>
+
+struct PlayerWeapon final
+{
+    bool active = false;
+    enum class Type
+    {
+        Pulse, Laser
+    }type = Type::Pulse;
+};
+
+class PlayerWeaponSystem final : public cro::System
+{
+public:
+    PlayerWeaponSystem(cro::MessageBus&);
+
+    void process(cro::Time) override;
+
+    void handleMessage(const cro::Message&) override;
+
+private:
+
+    void onEntityAdded(cro::Entity) override;
+
+    bool m_systemActive;
+    enum class FireMode
+    {
+        Single, Double, Triple, Laser, LaserWide
+    }m_fireMode;
+
+    std::vector<cro::int32> m_aliveList;
+    std::vector<cro::int32> m_deadPulses;
+    std::vector<cro::int32> m_deadLasers;
+};
+
+#endif //TL_PLAYER_WEAPONS_HPP_
