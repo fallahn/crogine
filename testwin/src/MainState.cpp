4aedcb491a16e1fb4f673326faf81387b49e1517
fallahn <matty_styles@hotmail.com>
2017-08-01 21:51:13 +0100

    test android build

--- testwin/src/MainState.cpp
+++ testwin/src/MainState.cpp
@@ -211,8 +211,8 @@ void MainState::loadAssets()
     menuFont.loadFromFile("assets/fonts/Audiowide-Regular.ttf");
 
     //audio
-    //m_resources.audio.load(AudioID::Test, "assets/audio/boop_loop.ogg");
-    m_resources.audio.load(AudioID::TestStream, "assets/audio/giblet_tickler.ogg", true);
+    m_resources.audio.load(AudioID::Test, "assets/audio/boop_loop.ogg");
+    m_resources.audio.load(AudioID::TestStream, "assets/audio/game.ogg", true);
 }
 
 void MainState::createScene()
