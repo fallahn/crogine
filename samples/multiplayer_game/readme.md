8c4c966e2de564f6c85aec6d76fd4bd9143adfa3
fallahn <matty_styles@hotmail.com>
2020-02-17 12:51:43 +0000

    add multiplayer game sample

---
+++ samples/multiplayer_game/readme.md
@@ -0,0 +1,4 @@
+Multiplayer Network Game
+------------------------
+
+Possibly.
---
+++ samples/multiplayer_game/src/MenuState.cpp
@@ -0,0 +1,102 @@
+/*-----------------------------------------------------------------------
+
+Matt Marchant 2020
+http://trederia.blogspot.com
+
+crogine application - Zlib license.
+
+This software is provided 'as-is', without any express or
+implied warranty.In no event will the authors be held
+liable for any damages arising from the use of this software.
+
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute
+it freely, subject to the following restrictions :
+
+1. The origin of this software must not be misrepresented;
+you must not claim that you wrote the original software.
+If you use this software in a product, an acknowledgment
+in the product documentation would be appreciated but
+is not required.
+
+2. Altered source versions must be plainly marked as such,
+and must not be misrepresented as being the original software.
+
+3. This notice may not be removed or altered from any
+source distribution.
+
+-----------------------------------------------------------------------*/
+
+#include "MenuState.hpp"
+
+#include <crogine/core/App.hpp>
+#include <crogine/gui/Gui.hpp>
+
+namespace
+{
+
+}
+
+MenuState::MenuState(cro::StateStack& stack, cro::State::Context context)
+	: cro::State    (stack, context),
+    m_scene         (context.appInstance.getMessageBus())
+{
+    //launches a loading screen (registered in MyApp.cpp)
+    context.mainWindow.loadResources([this]() {
+        //add systems to scene
+        addSystems();
+        //load assets (textures, shaders, models etc)
+        loadAssets();
+        //create some entities
+        createScene();
+    });
+
+    context.appInstance.setClearColour(cro::Colour(0.2f, 0.2f, 0.26f));
+}
+
+//public
+bool MenuState::handleEvent(const cro::Event& evt)
+{
+    if(cro::ui::wantsMouse() || cro::ui::wantsKeyboard())
+    {
+        return true;
+    }
+
+    m_scene.forwardEvent(evt);
+	return true;
+}
+
+void MenuState::handleMessage(const cro::Message& msg)
+{
+    m_scene.forwardMessage(msg);
+}
+
+bool MenuState::simulate(float dt)
+{
+    m_scene.simulate(dt);
+	return true;
+}
+
+void MenuState::render()
+{
+	//draw any renderable systems
+    m_scene.render();
+}
+
+//private
+void MenuState::addSystems()
+{
+    auto& mb = getContext().appInstance.getMessageBus();
+
+
+}
+
+void MenuState::loadAssets()
+{
+
+}
+
+void MenuState::createScene()
+{
+
+}
