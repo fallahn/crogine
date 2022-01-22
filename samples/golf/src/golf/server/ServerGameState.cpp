64621c25a3777ce77ce7f9424815c897aa77acb4
fallahn <matty_styles@hotmail.com>
2022-01-22 15:09:49 +0000

    tweak putt timing

--- samples/golf/src/golf/server/ServerGameState.cpp
+++ samples/golf/src/golf/server/ServerGameState.cpp
@@ -361,7 +361,7 @@ void GameState::handlePlayerInput(const cro::NetEvent::Packet& packet)
             //this is a kludge to wait for the anim before hitting the ball
             //Ideally we want to read the frame data from the avatar
             //as well as account for a frame of interp delay on the client
-            ball.delay = ball.terrain == TerrainID::Green ? 0.42f : 1.32f;
+            ball.delay = ball.terrain == TerrainID::Green ? 0.15f : 1.32f;
             //ball.delay = 0.32f;
             ball.startPoint = m_playerInfo[0].ballEntity.getComponent<cro::Transform>().getPosition();
 
