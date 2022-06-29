c0bcd4332aeaf5732efdc4380146beac3b6dc251
fallahn <matty_styles@hotmail.com>
2022-06-29 19:04:11 +0100

    lay out snooker balls

--- samples/golf/src/golf/server/SnookerDirector.cpp
+++ samples/golf/src/golf/server/SnookerDirector.cpp
@@ -36,8 +36,13 @@ SnookerDirector::SnookerDirector()
     m_firstCollision    (0),
     m_turnFlags         (0)
 {
+    //table length 3.265 / 1.6325
+    constexpr float TableLength = 3.265f;
+    constexpr float HalfLength = TableLength / 2.f;
+
+
     //red balls
-    static constexpr glm::vec3 Offset(0.f, BallHeight, 0.0935f - 0.465f); //distance to middle ball (TODO set this correctly)
+    static constexpr glm::vec3 Offset(0.f, BallHeight, -HalfLength * 0.55f); //distance to middle ball
 
     glm::vec3 basePos(0.f);
     const glm::vec3 Spacing(-0.027f, 0.f, -0.0468f);
@@ -57,12 +62,12 @@ SnookerDirector::SnookerDirector()
         basePos += Spacing;
     }
 
-    m_ballLayout.emplace_back(glm::vec3(0.3f, BallHeight, 0.f), 2); //0.2 * table length
-    m_ballLayout.emplace_back(glm::vec3(0.f, BallHeight, 0.f), 3);
-    m_ballLayout.emplace_back(glm::vec3(-0.3f, BallHeight, 0.f), 4);
+    m_ballLayout.emplace_back(glm::vec3(0.3f, BallHeight, HalfLength - (TableLength * 0.2f)), 2); //0.2 * table length
+    m_ballLayout.emplace_back(glm::vec3(0.f, BallHeight, HalfLength - (TableLength * 0.2f)), 3);
+    m_ballLayout.emplace_back(glm::vec3(-0.3f, BallHeight, HalfLength - (TableLength * 0.2f)), 4);
     m_ballLayout.emplace_back(glm::vec3(0.f, BallHeight, 0.f), 5); //perfect centre
-    m_ballLayout.emplace_back(glm::vec3(0.f, BallHeight, 0.f), 6); //0.25 table length
-    m_ballLayout.emplace_back(glm::vec3(0.f, BallHeight, 0.f), 7); //0.09 table length
+    m_ballLayout.emplace_back(glm::vec3(0.f, BallHeight, -HalfLength / 2.f), 6); //0.25 table length
+    m_ballLayout.emplace_back(glm::vec3(0.f, BallHeight, -(HalfLength - (TableLength * 0.09f))), 7); //0.09 table length
 }
 
 //public
