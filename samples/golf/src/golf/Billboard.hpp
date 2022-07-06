0400a6b735cfcfee6d75989ae0d0173904966a41
fallahn <matty_styles@hotmail.com>
2022-07-06 11:46:54 +0100

    begin re-scaling shrub sprites

--- samples/golf/src/golf/Billboard.hpp
+++ samples/golf/src/golf/Billboard.hpp
@@ -33,7 +33,7 @@ source distribution.
 #include <crogine/ecs/components/Sprite.hpp>
 #include <crogine/graphics/Texture.hpp>
 
-static constexpr float PixelPerMetre = 32.f;// 64.f; //used for scaling billboards
+static constexpr float PixelPerMetre = 32.f;// 64.f; //used for scaling clouds and 3D sprites
 
 struct BillboardID final
 {
@@ -60,13 +60,16 @@ struct BillboardID final
 
 static inline cro::Billboard spriteToBillboard(const cro::Sprite& sprite)
 {
+    //pixels per metre in a billboard - not the same as above
+    static constexpr float PPM = 64.f;
+
     if (sprite.getTexture())
     {
         auto bounds = sprite.getTextureRect();
         auto texSize = glm::vec2(sprite.getTexture()->getSize());
 
         cro::Billboard bb;
-        bb.size = { bounds.width / PixelPerMetre, bounds.height / PixelPerMetre };
+        bb.size = { bounds.width / PPM, bounds.height / PPM };
         bb.textureRect = { bounds.left / texSize.x, bounds.bottom / texSize.y, bounds.width / texSize.x, bounds.height / texSize.y };
         bb.origin = { bb.size.x / 2.f, 0.f };
         return bb;
