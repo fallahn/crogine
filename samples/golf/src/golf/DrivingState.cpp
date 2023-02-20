f711af0241011558e089adf9a405ac17220db013
fallahn <matty_styles@hotmail.com>
2023-02-20 10:14:22 +0000

    update driving range to larger power bar

--- samples/golf/src/golf/DrivingState.cpp
+++ samples/golf/src/golf/DrivingState.cpp
@@ -821,8 +821,8 @@ void DrivingState::loadAssets()
 
     //UI stuff
     spriteSheet.loadFromFile("assets/golf/sprites/ui.spt", m_resources.textures);
-    m_sprites[SpriteID::PowerBar] = spriteSheet.getSprite("power_bar");
-    m_sprites[SpriteID::PowerBarInner] = spriteSheet.getSprite("power_bar_inner");
+    m_sprites[SpriteID::PowerBar] = spriteSheet.getSprite("power_bar_wide");
+    m_sprites[SpriteID::PowerBarInner] = spriteSheet.getSprite("power_bar_inner_wide");
     m_sprites[SpriteID::HookBar] = spriteSheet.getSprite("hook_bar");
     m_sprites[SpriteID::WindIndicator] = spriteSheet.getSprite("wind_dir");
     m_sprites[SpriteID::WindSpeed] = spriteSheet.getSprite("wind_speed");
