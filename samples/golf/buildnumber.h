4af1e62a98d3a6f2a18d74ef9d358501a7b141e7
fallahn <matty_styles@hotmail.com>
2023-06-30 18:13:15 +0100

    update to libsdl 2.28

--- samples/golf/buildnumber.h
+++ samples/golf/buildnumber.h
@@ -3,7 +3,7 @@
 #ifndef BUILD_NUMBER_H_ 
 #define BUILD_NUMBER_H_ 
 
-#define BUILDNUMBER 359
-#define BUILDNUMBER_STR "359" 
+#define BUILDNUMBER 360
+#define BUILDNUMBER_STR "360" 
 
 #endif /* BUILD_NUMBER_H_ */
