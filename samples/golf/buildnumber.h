7fdded4e203f7ab35aae89c892c07eb3558915ac
fallahn <matty_styles@hotmail.com>
2023-08-02 20:32:13 +0100

    update to SDL 2.28.2

--- samples/golf/buildnumber.h
+++ samples/golf/buildnumber.h
@@ -3,7 +3,7 @@
 #ifndef BUILD_NUMBER_H_ 
 #define BUILD_NUMBER_H_ 
 
-#define BUILDNUMBER 1064
-#define BUILDNUMBER_STR "1064" 
+#define BUILDNUMBER 1066
+#define BUILDNUMBER_STR "1066" 
 
 #endif /* BUILD_NUMBER_H_ */
