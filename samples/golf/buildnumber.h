eb61e32ed8eb567e3d8b8d3612f50bd958d46f10
fallahn <matty_styles@hotmail.com>
2023-10-04 10:44:40 +0100

    increase minimum macos version to 10.15 use std::filesystem on macos

--- samples/golf/buildnumber.h
+++ samples/golf/buildnumber.h
@@ -3,7 +3,7 @@
 #ifndef BUILD_NUMBER_H_ 
 #define BUILD_NUMBER_H_ 
 
-#define BUILDNUMBER 1926
-#define BUILDNUMBER_STR "1926" 
+#define BUILDNUMBER 1927
+#define BUILDNUMBER_STR "1927" 
 
 #endif /* BUILD_NUMBER_H_ */
