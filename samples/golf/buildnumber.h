32da26f25a523425507494738340c7ede1dc8d81
fallahn <matty_styles@hotmail.com>
2023-10-06 10:22:18 +0100

    move challenge notification to top right corner

--- samples/golf/buildnumber.h
+++ samples/golf/buildnumber.h
@@ -3,7 +3,7 @@
 #ifndef BUILD_NUMBER_H_ 
 #define BUILD_NUMBER_H_ 
 
-#define BUILDNUMBER 1933
-#define BUILDNUMBER_STR "1933" 
+#define BUILDNUMBER 1934
+#define BUILDNUMBER_STR "1934" 
 
 #endif /* BUILD_NUMBER_H_ */
