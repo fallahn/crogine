3b519d1e01451a33af14b2b388fcb8c25b8498ce
fallahn <matty_styles@hotmail.com>
2023-10-26 09:35:16 +0100

    update macOS build path

--- samples/golf/macOS/build.sh
+++ samples/golf/macOS/build.sh
@@ -1,26 +1,28 @@
 #!/usr/bin/env zsh
 
 # build crogine
-cd ./crogine*/
+cd ../../../crogine*/
 mkdir build
 cd build
 cmake ..
 sudo -A make install
 
 # build golf
-cd ../../../samples/golf
+cd ..
 # make sure assets have been copied into golf from latest Windows download
 mkdir build
 cd build
 cmake .. -D MACOS_BUNDLE=true -G Xcode
 sudo -A cmake --build . --config Release --target install
 
+cd ..
+
 # set permissions
-sudo -A chown -Rv $USER Release
+sudo -A chown -Rv $USER bundle
 
 # ad-hoc code sign
-codesign --force -s - Release/golf.app
+codesign --force -s - bundle/golf.app
 
 # replace app
 rm -rf /Applications/golf.app
-cp -R Release/golf.app /Applications/golf.app
+cp -R bundle/golf.app /Applications/golf.app
