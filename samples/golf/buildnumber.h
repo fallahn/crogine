b9b089ea3a070d6a3621bdbac46392c54775e969
fallahn <matty_styles@hotmail.com>
2023-10-31 10:12:49 +0000

    use freetype renderer with imgui

--- samples/golf/buildnumber.h
+++ samples/golf/buildnumber.h
@@ -3,7 +3,7 @@
 #ifndef BUILD_NUMBER_H_ 
 #define BUILD_NUMBER_H_ 
 
-#define BUILDNUMBER 2269
-#define BUILDNUMBER_STR "2269" 
+#define BUILDNUMBER 2271
+#define BUILDNUMBER_STR "2271" 
 
 #endif /* BUILD_NUMBER_H_ */
