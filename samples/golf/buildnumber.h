1f9f48f3888361df8669fd9251ae63cb8acb33b0
fallahn <matty_styles@hotmail.com>
2023-11-22 12:26:15 +0000

    tweak wind shader

--- samples/golf/buildnumber.h
+++ samples/golf/buildnumber.h
@@ -3,7 +3,7 @@
 #ifndef BUILD_NUMBER_H_ 
 #define BUILD_NUMBER_H_ 
 
-#define BUILDNUMBER 2814
-#define BUILDNUMBER_STR "2814" 
+#define BUILDNUMBER 2816
+#define BUILDNUMBER_STR "2816" 
 
 #endif /* BUILD_NUMBER_H_ */
