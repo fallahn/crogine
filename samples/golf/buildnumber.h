eb0220b99090c142c89aa5d902a6d378cbb92c53
fallahn <matty_styles@hotmail.com>
2023-11-28 10:56:26 +0000

    fix MRT indexing of overview map

--- samples/golf/buildnumber.h
+++ samples/golf/buildnumber.h
@@ -3,7 +3,7 @@
 #ifndef BUILD_NUMBER_H_ 
 #define BUILD_NUMBER_H_ 
 
-#define BUILDNUMBER 2939
-#define BUILDNUMBER_STR "2939" 
+#define BUILDNUMBER 2941
+#define BUILDNUMBER_STR "2941" 
 
 #endif /* BUILD_NUMBER_H_ */
