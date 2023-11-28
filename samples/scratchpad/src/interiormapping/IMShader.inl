6aca1d33136599d347ec7cd204f9277fb4870704
fallahn <matty_styles@hotmail.com>
2023-11-28 16:17:19 +0000

    experiments in interior mapping

--- samples/scratchpad/src/interiormapping/IMShader.inl
+++ samples/scratchpad/src/interiormapping/IMShader.inl
@@ -3,3 +3,75 @@ Tangent space interior mapping shader based on https://andrewgotow.com/2018/09/0
 */
 
 #pragma once
+
+#include <string>
+
+inline const std::string IMVertex = 
+R"(
+ATTRIBUTE vec4 a_position;
+ATTRIBUTE vec3 a_normal;
+ATTRIBUTE vec3 a_tangent;
+//ATTRIBUTE vec3 a_bitangent;
+ATTRIBUTE vec2 a_texCoord0;
+
+uniform mat4 u_worldMatrix;
+uniform mat4 u_viewProjectionMatrix;
+uniform mat3 u_normalMatrix;
+uniform vec3 u_cameraWorldPosition;
+
+VARYING_OUT vec2 v_tanPosition;
+VARYING_OUT vec3 v_tanEyeDir;
+
+void main()
+{
+    vec4 worldPos = u_worldMatrix * a_position;
+    vec3 eyeVec = worldPos.xyz - u_cameraWorldPosition;
+
+//hum. fix this - or at least find the sign
+vec3 bitan = /*normalize*/(cross(a_normal, a_tangent));
+
+    mat3 tanMatrix = 
+    mat3(
+        normalize(u_normalMatrix * a_tangent),
+        normalize(u_normalMatrix * bitan),
+        normalize(u_normalMatrix * a_normal)
+    );
+    v_tanEyeDir = transpose(tanMatrix) * eyeVec;
+
+    v_tanPosition = a_texCoord0;
+    gl_Position = u_viewProjectionMatrix * worldPos;
+}
+)";
+
+inline const std::string IMFragment = 
+R"(
+OUTPUT
+
+uniform sampler2D u_roomTexture;
+
+VARYING_IN vec2 v_tanPosition;
+VARYING_IN vec3 v_tanEyeDir;
+
+const vec3 RoomSize = vec3(1.0, 1.0, 1.0); //TODO we could encode this in the vertex colour
+const float BackPlaneScale = 0.5;
+
+void main()
+{
+    vec3 rayStart = fract(vec3(v_tanPosition, 0.0) / RoomSize);
+    vec3 rayDir = normalize(v_tanEyeDir) / RoomSize;
+
+    vec3 boxMin = floor(vec3(v_tanPosition, -1.0));
+    vec3 boxMid = boxMin + vec3(0.5);
+    vec3 boxMax = boxMin + vec3(1.0);
+
+    vec3 planes = mix(boxMin, boxMax, step(0.0, rayDir));
+    vec3 tanPlane = (planes - rayStart) / rayDir;
+
+    float tanDist = min(min(tanPlane.x, tanPlane.y), tanPlane.z);
+
+    vec3 roomRay = ((rayDir * tanDist) + rayStart) - boxMid;
+    vec2 roomUV = (roomRay.xy * mix(BackPlaneScale, 1.0, roomRay.z + 0.5)) + 0.5;
+
+    FRAG_OUT = vec4(TEXTURE(u_roomTexture, roomUV).rgb, 1.0);
+}
+)";
--- samples/scratchpad/src/interiormapping/InteriormappingState.cpp
+++ samples/scratchpad/src/interiormapping/InteriormappingState.cpp
@@ -16,7 +16,34 @@
 #include <crogine/ecs/systems/SpriteSystem2D.hpp>
 #include <crogine/ecs/systems/RenderSystem2D.hpp>
 
+#include <crogine/graphics/BinaryMeshBuilder.hpp>
+
 #include <crogine/util/Constants.hpp>
+#include <crogine/detail/glm/gtx/euler_angles.hpp>
+
+namespace
+{
+#include "IMShader.inl"
+
+    struct ShaderID final
+    {
+        enum
+        {
+            IntMapping,
+
+            Count
+        };
+    };
+    std::array<std::uint32_t, ShaderID::Count> shaderIDs = {};
+
+    struct MaterialID final
+    {
+        enum
+        {
+            InMapping = 1
+        };
+    };
+}
 
 InteriorMappingState::InteriorMappingState(cro::StateStack& stack, cro::State::Context context)
     : cro::State    (stack, context),
@@ -91,26 +118,110 @@ void InteriorMappingState::addSystems()
 
 void InteriorMappingState::loadAssets()
 {
+    m_resources.shaders.loadFromString(ShaderID::IntMapping, IMVertex, IMFragment);
+    m_resources.materials.add(MaterialID::InMapping, m_resources.shaders.get(ShaderID::IntMapping));
+
+    m_resources.materials.get(MaterialID::InMapping).setProperty("u_roomTexture", m_resources.textures.get("assets/images/interior.png"));
 }
 
 void InteriorMappingState::createScene()
 {
     cro::ModelDefinition md(m_resources);
-    if (md.loadFromFile("assets/models/cylinder_tangents.cmt"))
+    //if (md.loadFromFile("assets/models/cylinder_tangents.cmt"))
+    //{
+    //    auto entity = m_gameScene.createEntity();
+    //    entity.addComponent<cro::Transform>().setPosition({ -1.5, 0.f, 0.f });
+    //    md.createModel(entity);
+
+    //    entity.getComponent<cro::Model>().setMaterial(0, m_resources.materials.get(MaterialID::InMapping));
+
+    //    //TODO refactor this to a sngle func that take the entity as a param
+    //    registerWindow([&, entity]() mutable
+    //        {
+    //            if (ImGui::Begin("Cylinder"))
+    //            {
+    //                auto pos = entity.getComponent<cro::Transform>().getPosition();
+
+    //                if (ImGui::SliderFloat("X", &pos.x, -3.f, 3.f))
+    //                {
+    //                    pos.x = std::clamp(pos.x, -3.f, 3.f);
+    //                    entity.getComponent<cro::Transform>().setPosition(pos);
+    //                }
+
+    //                if (ImGui::SliderFloat("Y", &pos.y, -2.f, 2.f))
+    //                {
+    //                    pos.y = std::clamp(pos.y, -2.f, 2.f);
+    //                    entity.getComponent<cro::Transform>().setPosition(pos);
+    //                }
+
+    //                static glm::vec3 rotation = glm::vec3(0.f);
+    //                if (ImGui::SliderFloat("Rotation X", &rotation[0], -cro::Util::Const::PI, cro::Util::Const::PI))
+    //                {
+    //                    rotation.x = std::clamp(rotation[0], -cro::Util::Const::PI, cro::Util::Const::PI);
+    //                    entity.getComponent<cro::Transform>().setRotation(glm::toQuat(glm::orientate3(rotation)));
+    //                }
+    //            }
+    //            ImGui::End();
+    //        });
+    //}
+
+    if (md.loadFromFile("assets/models/plane_tangents.cmt"))
     {
         auto entity = m_gameScene.createEntity();
         entity.addComponent<cro::Transform>();
         md.createModel(entity);
+        entity.getComponent<cro::Model>().setMaterial(0, m_resources.materials.get(MaterialID::InMapping));
 
-
-        entity.addComponent<cro::Callback>().active = true;
-        entity.getComponent<cro::Callback>().function =
-            [](cro::Entity e, float dt)
+        registerWindow([&, entity]() mutable
             {
-                e.getComponent<cro::Transform>().rotate(cro::Transform::Y_AXIS, dt * 0.5f);
-            };
-    }
+                if (ImGui::Begin("Plane"))
+                {
+                    auto pos = entity.getComponent<cro::Transform>().getPosition();
+                    
+                    if (ImGui::SliderFloat("X", &pos.x, -3.f, 3.f))
+                    {
+                        pos.x = std::clamp(pos.x, -3.f, 3.f);
+                        entity.getComponent<cro::Transform>().setPosition(pos);
+                    }
+                    
+                    if (ImGui::SliderFloat("Y", &pos.y, -2.f, 2.f))
+                    {
+                        pos.y = std::clamp(pos.y, -2.f, 2.f);
+                        entity.getComponent<cro::Transform>().setPosition(pos);
+                    }
 
+                    static glm::vec3 rotation = glm::vec3(0.f);
+                    if (ImGui::SliderFloat("Rotation X", &rotation[0], -cro::Util::Const::PI, cro::Util::Const::PI))
+                    {
+                        rotation.x = std::clamp(rotation[0], -cro::Util::Const::PI, cro::Util::Const::PI);
+                        entity.getComponent<cro::Transform>().setRotation(glm::toQuat(glm::orientate3(rotation)));
+                    }
+                    if (ImGui::SliderFloat("Rotation Y", &rotation[2], -cro::Util::Const::PI, cro::Util::Const::PI))
+                    {
+                        rotation.z = std::clamp(rotation[2], -cro::Util::Const::PI, cro::Util::Const::PI);
+                        entity.getComponent<cro::Transform>().setRotation(glm::toQuat(glm::orientate3(rotation)));
+                    }
+                    if (ImGui::SliderFloat("Rotation Z", &rotation[1], -cro::Util::Const::PI, cro::Util::Const::PI))
+                    {
+                        rotation.y = std::clamp(rotation[1], -cro::Util::Const::PI, cro::Util::Const::PI);
+                        entity.getComponent<cro::Transform>().setRotation(glm::toQuat(glm::orientate3(rotation)));
+                    }
+                    if (ImGui::Button("Reset"))
+                    {
+                        rotation = glm::vec3(0.f);
+                        entity.getComponent<cro::Transform>().setRotation(glm::toQuat(glm::orientate3(rotation)));
+                    }
+
+                    static glm::vec3 scale(1.f);
+                    if (ImGui::SliderFloat("Scale X", &scale.x, 0.5f, 2.f))
+                    {
+                        scale.x = std::clamp(scale.x, 0.5f, 2.f);
+                        entity.getComponent<cro::Transform>().setScale(scale);
+                    }
+                }
+                ImGui::End();
+            });
+    }
 
     auto resize = [](cro::Camera& cam)
     {
@@ -123,8 +234,8 @@ void InteriorMappingState::createScene()
     cam.resizeCallback = resize;
     resize(cam);
 
-    m_gameScene.getActiveCamera().getComponent<cro::Transform>().setPosition({ 0.f, 2.8f, 3.5f });
-    m_gameScene.getActiveCamera().getComponent<cro::Transform>().rotate(cro::Transform::X_AXIS, -0.4f);
+    m_gameScene.getActiveCamera().getComponent<cro::Transform>().setPosition({ 0.f, 1.f, 3.5f });
+    //m_gameScene.getActiveCamera().getComponent<cro::Transform>().rotate(cro::Transform::X_AXIS, -0.4f);
 }
 
 void InteriorMappingState::createUI()
