75848c988d45f1797e4b9a192da687c2fa7d3311
fallahn <matty_styles@hotmail.com>
2024-01-02 15:56:44 +0000

    increase max entity generations from 1k to 64k

--- samples/golf/buildnumber.h
+++ samples/golf/buildnumber.h
@@ -3,7 +3,7 @@
 #ifndef BUILD_NUMBER_H_ 
 #define BUILD_NUMBER_H_ 
 
-#define BUILDNUMBER 3657
-#define BUILDNUMBER_STR "3657" 
+#define BUILDNUMBER 3659
+#define BUILDNUMBER_STR "3659" 
 
 #endif /* BUILD_NUMBER_H_ */
