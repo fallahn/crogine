a7436911bb7b06cfce7baf564bc68d7d7df009b3
fallahn <matty_styles@hotmail.com>
2024-01-08 16:07:23 +0000

    shenanigans with road rendering

--- samples/scratchpad/src/endless/EndlessDrivingState.hpp
+++ samples/scratchpad/src/endless/EndlessDrivingState.hpp
@@ -24,9 +24,17 @@ public:
 
 private:
 
+    //vehicle is a 3D model in its own scene
+    //rendered to a buffer to use as a sprite
+    cro::Scene m_playerScene;
     cro::Scene m_gameScene;
     cro::Scene m_uiScene;
     cro::ResourceCollection m_resources;
+
+    //buffer for player sprite
+    cro::RenderTexture m_playerTexture;
+    cro::Entity m_playerEntity;
+    cro::Entity m_roadEntity;
 
     //texture is fixed size and game is rendered to this
     //the ui scene then scales this to the current output
@@ -35,6 +43,7 @@ private:
 
     void addSystems();
     void loadAssets();
+    void createPlayer();
     void createScene();
     void createUI();
 
