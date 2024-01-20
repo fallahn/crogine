f42f1f8d750b0225ac44d4824b6fef8d40bb3520
fallahn <matty_styles@hotmail.com>
2024-01-20 15:04:36 +0000

    raise trigger events for water, scrub and bunker types

--- samples/golf/buildnumber.h
+++ samples/golf/buildnumber.h
@@ -3,7 +3,7 @@
 #ifndef BUILD_NUMBER_H_ 
 #define BUILD_NUMBER_H_ 
 
-#define BUILDNUMBER 3825
-#define BUILDNUMBER_STR "3825" 
+#define BUILDNUMBER 3830
+#define BUILDNUMBER_STR "3830" 
 
 #endif /* BUILD_NUMBER_H_ */
