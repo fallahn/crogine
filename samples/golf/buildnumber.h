5f3fa254bb869f5ca40bfef71aed6c05fe5729dd
fallahn <matty_styles@hotmail.com>
2024-01-30 11:35:33 +0000

    remove redundant tree queries from ModelRenderer

--- samples/golf/buildnumber.h
+++ samples/golf/buildnumber.h
@@ -3,7 +3,7 @@
 #ifndef BUILD_NUMBER_H_ 
 #define BUILD_NUMBER_H_ 
 
-#define BUILDNUMBER 3902
-#define BUILDNUMBER_STR "3902" 
+#define BUILDNUMBER 3903
+#define BUILDNUMBER_STR "3903" 
 
 #endif /* BUILD_NUMBER_H_ */
