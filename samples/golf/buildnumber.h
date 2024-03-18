8e5fc58e6fd4d67c88e499c6723f44e24372d0ac
fallahn <matty_styles@hotmail.com>
2024-03-18 15:18:23 +0000

    remove unused var

--- samples/golf/buildnumber.h
+++ samples/golf/buildnumber.h
@@ -3,7 +3,7 @@
 #ifndef BUILD_NUMBER_H_ 
 #define BUILD_NUMBER_H_ 
 
-#define BUILDNUMBER 4975
-#define BUILDNUMBER_STR "4975" 
+#define BUILDNUMBER 4977
+#define BUILDNUMBER_STR "4977" 
 
 #endif /* BUILD_NUMBER_H_ */
