fa0fa906e80303fbad335051e01ada8930592170
fallahn <matty_styles@hotmail.com>
2024-03-21 15:40:32 +0000

    cloud sync career.cfg

--- samples/golf/buildnumber.h
+++ samples/golf/buildnumber.h
@@ -3,7 +3,7 @@
 #ifndef BUILD_NUMBER_H_ 
 #define BUILD_NUMBER_H_ 
 
-#define BUILDNUMBER 5078
-#define BUILDNUMBER_STR "5078" 
+#define BUILDNUMBER 5082
+#define BUILDNUMBER_STR "5082" 
 
 #endif /* BUILD_NUMBER_H_ */
