22b9bb919042d8e8417951c28c0ef5d6072fc456
fallahn <matty_styles@hotmail.com>
2024-03-27 12:36:12 +0000

    update ball format documentation

--- samples/golf/buildnumber.h
+++ samples/golf/buildnumber.h
@@ -3,7 +3,7 @@
 #ifndef BUILD_NUMBER_H_ 
 #define BUILD_NUMBER_H_ 
 
-#define BUILDNUMBER 5142
-#define BUILDNUMBER_STR "5142" 
+#define BUILDNUMBER 5143
+#define BUILDNUMBER_STR "5143" 
 
 #endif /* BUILD_NUMBER_H_ */
