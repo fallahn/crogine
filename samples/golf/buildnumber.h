b4c7a1fb43e2c960e6c9c0d728211aa9ccb5230a
fallahn <matty_styles@hotmail.com>
2024-04-02 19:11:14 +0100

    update assets

--- samples/golf/buildnumber.h
+++ samples/golf/buildnumber.h
@@ -3,7 +3,7 @@
 #ifndef BUILD_NUMBER_H_ 
 #define BUILD_NUMBER_H_ 
 
-#define BUILDNUMBER 5165
-#define BUILDNUMBER_STR "5165" 
+#define BUILDNUMBER 5167
+#define BUILDNUMBER_STR "5167" 
 
 #endif /* BUILD_NUMBER_H_ */
