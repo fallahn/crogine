97ab8c486d386003b3ca3e2fd5613ad302898ca6
fallahn <matty_styles@hotmail.com>
2024-05-02 10:56:10 +0100

    bump version number

--- libsocial/include/Social.hpp
+++ libsocial/include/Social.hpp
@@ -49,11 +49,11 @@ source distribution.
 //(ball started sending wind effect 1120 -> 1124)
 //(added night mode/weather 1141 -> 1150)
 //(player avatar data format changed 1153->1160)
-static constexpr std::uint16_t CURRENT_VER = 1164;
+static constexpr std::uint16_t CURRENT_VER = 1170;
 #ifdef __APPLE__
-static const std::string StringVer("1.16.4 (macOS beta)");
+static const std::string StringVer("1.17.0 (macOS beta)");
 #else
-static const std::string StringVer("1.16.4");
+static const std::string StringVer("1.17.0");
 #endif
 
 struct HallEntry final
