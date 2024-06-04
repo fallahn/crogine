fdbfb8142b3cd0ffefb777c4e749f4c850af0eba
fallahn <matty_styles@hotmail.com>
2024-06-04 13:18:57 +0100

    don't show happy snapper progress on courses already snapped

--- samples/golf/buildnumber.h
+++ samples/golf/buildnumber.h
@@ -3,7 +3,7 @@
 #ifndef BUILD_NUMBER_H_ 
 #define BUILD_NUMBER_H_ 
 
-#define BUILDNUMBER 6238
-#define BUILDNUMBER_STR "6238" 
+#define BUILDNUMBER 6239
+#define BUILDNUMBER_STR "6239" 
 
 #endif /* BUILD_NUMBER_H_ */
