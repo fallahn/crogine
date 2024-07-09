5954a557586e81fae9028e63bdc1839880e80e79
fallahn <matty_styles@hotmail.com>
2024-07-09 12:00:41 +0100

    prevent mouse input when window loses focus

--- samples/golf/buildnumber.h
+++ samples/golf/buildnumber.h
@@ -3,7 +3,7 @@
 #ifndef BUILD_NUMBER_H_ 
 #define BUILD_NUMBER_H_ 
 
-#define BUILDNUMBER 6766
-#define BUILDNUMBER_STR "6766" 
+#define BUILDNUMBER 6767
+#define BUILDNUMBER_STR "6767" 
 
 #endif /* BUILD_NUMBER_H_ */
