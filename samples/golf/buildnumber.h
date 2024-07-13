8c5097a5487bc3ad504f3beca0b74c4967440bcc
fallahn <matty_styles@hotmail.com>
2024-07-13 11:16:54 +0100

    fix overlapping text in league menu

--- samples/golf/buildnumber.h
+++ samples/golf/buildnumber.h
@@ -3,7 +3,7 @@
 #ifndef BUILD_NUMBER_H_ 
 #define BUILD_NUMBER_H_ 
 
-#define BUILDNUMBER 6794
-#define BUILDNUMBER_STR "6794" 
+#define BUILDNUMBER 6803
+#define BUILDNUMBER_STR "6803" 
 
 #endif /* BUILD_NUMBER_H_ */
