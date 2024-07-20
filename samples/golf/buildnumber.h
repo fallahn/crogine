c3c3b51e8f945b7eaa09a793a5835f07b4f6e24c
fallahn <matty_styles@hotmail.com>
2024-07-20 15:00:24 +0100

    re-calibrate stroke indicator markings

--- samples/golf/buildnumber.h
+++ samples/golf/buildnumber.h
@@ -3,7 +3,7 @@
 #ifndef BUILD_NUMBER_H_ 
 #define BUILD_NUMBER_H_ 
 
-#define BUILDNUMBER 6906
-#define BUILDNUMBER_STR "6906" 
+#define BUILDNUMBER 6908
+#define BUILDNUMBER_STR "6908" 
 
 #endif /* BUILD_NUMBER_H_ */
--- samples/golf/src/golf/GameConsts.hpp
+++ samples/golf/src/golf/GameConsts.hpp
@@ -610,44 +610,44 @@ static inline std::vector<cro::Vertex2D> getStrokeIndicatorVerts()
         cro::Vertex2D(glm::vec2(0.f, -0.5f), TextGoldColour),
 
         //grey
-        cro::Vertex2D(glm::vec2(0.0575f, 0.5f), TextGoldColour),
-        cro::Vertex2D(glm::vec2(0.0575f, -0.5f), TextGoldColour),
+        cro::Vertex2D(glm::vec2(0.0188f, 0.5f), TextGoldColour),
+        cro::Vertex2D(glm::vec2(0.0188f, -0.5f), TextGoldColour),
 
-        cro::Vertex2D(glm::vec2(0.0575f, 0.5f), Grey),
-        cro::Vertex2D(glm::vec2(0.0575f, -0.5f), Grey),
+        cro::Vertex2D(glm::vec2(0.0188f, 0.5f), Grey),
+        cro::Vertex2D(glm::vec2(0.0188f, -0.5f), Grey),
 
-        cro::Vertex2D(glm::vec2(0.0675f, 0.5f), Grey),
-        cro::Vertex2D(glm::vec2(0.0675f, -0.5f), Grey),
+        cro::Vertex2D(glm::vec2(0.0288f, 0.5f), Grey),
+        cro::Vertex2D(glm::vec2(0.0288f, -0.5f), Grey),
 
-        cro::Vertex2D(glm::vec2(0.0675f, 0.5f), TextGoldColour),
-        cro::Vertex2D(glm::vec2(0.0675f, -0.5f), TextGoldColour),
+        cro::Vertex2D(glm::vec2(0.0288f, 0.5f), TextGoldColour),
+        cro::Vertex2D(glm::vec2(0.0288f, -0.5f), TextGoldColour),
 
 
         //black
-        cro::Vertex2D(glm::vec2(0.12f, 0.5f), TextGoldColour),
-        cro::Vertex2D(glm::vec2(0.12f, -0.5f), TextGoldColour),
+        cro::Vertex2D(glm::vec2(0.07f, 0.5f), TextGoldColour),
+        cro::Vertex2D(glm::vec2(0.07f, -0.5f), TextGoldColour),
 
-        cro::Vertex2D(glm::vec2(0.12f, 0.5f), LeaderboardTextDark),
-        cro::Vertex2D(glm::vec2(0.12f, -0.5f), LeaderboardTextDark),
+        cro::Vertex2D(glm::vec2(0.07f, 0.5f), LeaderboardTextDark),
+        cro::Vertex2D(glm::vec2(0.07f, -0.5f), LeaderboardTextDark),
 
-        cro::Vertex2D(glm::vec2(0.13f, 0.5f), LeaderboardTextDark),
-        cro::Vertex2D(glm::vec2(0.13f, -0.5f), LeaderboardTextDark),
+        cro::Vertex2D(glm::vec2(0.08f, 0.5f), LeaderboardTextDark),
+        cro::Vertex2D(glm::vec2(0.08f, -0.5f), LeaderboardTextDark),
 
-        cro::Vertex2D(glm::vec2(0.13f, 0.5f), TextGoldColour),
-        cro::Vertex2D(glm::vec2(0.13f, -0.5f), TextGoldColour),
+        cro::Vertex2D(glm::vec2(0.08f, 0.5f), TextGoldColour),
+        cro::Vertex2D(glm::vec2(0.08f, -0.5f), TextGoldColour),
 
         //grey
-        cro::Vertex2D(glm::vec2(0.1825f, 0.5f), TextGoldColour),
-        cro::Vertex2D(glm::vec2(0.1825f, -0.5f), TextGoldColour),
+        cro::Vertex2D(glm::vec2(0.1525f, 0.5f), TextGoldColour),
+        cro::Vertex2D(glm::vec2(0.1525f, -0.5f), TextGoldColour),
 
-        cro::Vertex2D(glm::vec2(0.1825f, 0.5f), Grey),
-        cro::Vertex2D(glm::vec2(0.1825f, -0.5f), Grey),
+        cro::Vertex2D(glm::vec2(0.1525f, 0.5f), Grey),
+        cro::Vertex2D(glm::vec2(0.1525f, -0.5f), Grey),
 
-        cro::Vertex2D(glm::vec2(0.1925f, 0.5f), Grey),
-        cro::Vertex2D(glm::vec2(0.1925f, -0.5f), Grey),
+        cro::Vertex2D(glm::vec2(0.1625f, 0.5f), Grey),
+        cro::Vertex2D(glm::vec2(0.1625f, -0.5f), Grey),
 
-        cro::Vertex2D(glm::vec2(0.1925f, 0.5f), TextGoldColour),
-        cro::Vertex2D(glm::vec2(0.1925f, -0.5f), TextGoldColour),
+        cro::Vertex2D(glm::vec2(0.1625f, 0.5f), TextGoldColour),
+        cro::Vertex2D(glm::vec2(0.1625f, -0.5f), TextGoldColour),
 
         //black
         cro::Vertex2D(glm::vec2(0.245f, 0.5f), TextGoldColour),
@@ -664,44 +664,44 @@ static inline std::vector<cro::Vertex2D> getStrokeIndicatorVerts()
 
 
         //grey
-        cro::Vertex2D(glm::vec2(0.3075f, 0.5f), TextGoldColour),
-        cro::Vertex2D(glm::vec2(0.3075f, -0.5f), TextGoldColour),
-
-        cro::Vertex2D(glm::vec2(0.3075f, 0.5f), Grey),
-        cro::Vertex2D(glm::vec2(0.3075f, -0.5f), Grey),
+        cro::Vertex2D(glm::vec2(0.3175f, 0.5f), TextGoldColour),
+        cro::Vertex2D(glm::vec2(0.3175f, -0.5f), TextGoldColour),
 
         cro::Vertex2D(glm::vec2(0.3175f, 0.5f), Grey),
         cro::Vertex2D(glm::vec2(0.3175f, -0.5f), Grey),
 
-        cro::Vertex2D(glm::vec2(0.3175f, 0.5f), TextGoldColour),
-        cro::Vertex2D(glm::vec2(0.3175f, -0.5f), TextGoldColour),
+        cro::Vertex2D(glm::vec2(0.3275f, 0.5f), Grey),
+        cro::Vertex2D(glm::vec2(0.3275f, -0.5f), Grey),
+
+        cro::Vertex2D(glm::vec2(0.3275f, 0.5f), TextGoldColour),
+        cro::Vertex2D(glm::vec2(0.3275f, -0.5f), TextGoldColour),
 
 
         //black
-        cro::Vertex2D(glm::vec2(0.37f, 0.5f), TextGoldColour),
-        cro::Vertex2D(glm::vec2(0.37f, -0.5f), TextGoldColour),
+        cro::Vertex2D(glm::vec2(0.395f, 0.5f), TextGoldColour),
+        cro::Vertex2D(glm::vec2(0.395f, -0.5f), TextGoldColour),
 
-        cro::Vertex2D(glm::vec2(0.37f, 0.5f), LeaderboardTextDark),
-        cro::Vertex2D(glm::vec2(0.37f, -0.5f), LeaderboardTextDark),
+        cro::Vertex2D(glm::vec2(0.395f, 0.5f), LeaderboardTextDark),
+        cro::Vertex2D(glm::vec2(0.395f, -0.5f), LeaderboardTextDark),
 
-        cro::Vertex2D(glm::vec2(0.38f, 0.5f), LeaderboardTextDark),
-        cro::Vertex2D(glm::vec2(0.38f, -0.5f), LeaderboardTextDark),
+        cro::Vertex2D(glm::vec2(0.405f, 0.5f), LeaderboardTextDark),
+        cro::Vertex2D(glm::vec2(0.405f, -0.5f), LeaderboardTextDark),
 
-        cro::Vertex2D(glm::vec2(0.38f, 0.5f), TextGoldColour),
-        cro::Vertex2D(glm::vec2(0.38f, -0.5f), TextGoldColour),
+        cro::Vertex2D(glm::vec2(0.405f, 0.5f), TextGoldColour),
+        cro::Vertex2D(glm::vec2(0.405f, -0.5f), TextGoldColour),
 
         //grey
-        cro::Vertex2D(glm::vec2(0.4325f, 0.5f), TextGoldColour),
-        cro::Vertex2D(glm::vec2(0.4325f, -0.5f), TextGoldColour),
+        cro::Vertex2D(glm::vec2(0.4475f, 0.5f), TextGoldColour),
+        cro::Vertex2D(glm::vec2(0.4475f, -0.5f), TextGoldColour),
 
-        cro::Vertex2D(glm::vec2(0.4325f, 0.5f), Grey),
-        cro::Vertex2D(glm::vec2(0.4325f, -0.5f), Grey),
+        cro::Vertex2D(glm::vec2(0.4475f, 0.5f), Grey),
+        cro::Vertex2D(glm::vec2(0.4475f, -0.5f), Grey),
 
-        cro::Vertex2D(glm::vec2(0.4425f, 0.5f), Grey),
-        cro::Vertex2D(glm::vec2(0.4425f, -0.5f), Grey),
+        cro::Vertex2D(glm::vec2(0.4575f, 0.5f), Grey),
+        cro::Vertex2D(glm::vec2(0.4575f, -0.5f), Grey),
 
-        cro::Vertex2D(glm::vec2(0.4425f, 0.5f), TextGoldColour),
-        cro::Vertex2D(glm::vec2(0.4425f, -0.5f), TextGoldColour),
+        cro::Vertex2D(glm::vec2(0.4575f, 0.5f), TextGoldColour),
+        cro::Vertex2D(glm::vec2(0.4575f, -0.5f), TextGoldColour),
 
         //gold
         cro::Vertex2D(glm::vec2(0.5f, 0.5f), endColour),
