b146017d1f4ec79d73a33e8057181beb599a3997
fallahn <matty_styles@hotmail.com>
2024-09-05 20:11:35 +0100

    update text alignment

--- samples/golf/buildnumber.h
+++ samples/golf/buildnumber.h
@@ -3,7 +3,7 @@
 #ifndef BUILD_NUMBER_H_ 
 #define BUILD_NUMBER_H_ 
 
-#define BUILDNUMBER 7152
-#define BUILDNUMBER_STR "7152" 
+#define BUILDNUMBER 7154
+#define BUILDNUMBER_STR "7154" 
 
 #endif /* BUILD_NUMBER_H_ */
