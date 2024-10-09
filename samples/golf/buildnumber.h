38d77c3e080b1cbed7d40b5bcea1464183104c90
fallahn <matty_styles@hotmail.com>
2024-10-09 15:28:48 +0100

    update libsdl

--- samples/golf/buildnumber.h
+++ samples/golf/buildnumber.h
@@ -3,7 +3,7 @@
 #ifndef BUILD_NUMBER_H_ 
 #define BUILD_NUMBER_H_ 
 
-#define BUILDNUMBER 7174
-#define BUILDNUMBER_STR "7174" 
+#define BUILDNUMBER 7175
+#define BUILDNUMBER_STR "7175" 
 
 #endif /* BUILD_NUMBER_H_ */
