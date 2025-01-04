4abfe3bc281c7e8533689400376ce0a5fadf88e4
fallahn <matty_styles@hotmail.com>
2025-01-04 14:43:14 +0000

    update copyright dates

--- samples/golf/buildnumber.h
+++ samples/golf/buildnumber.h
@@ -3,7 +3,7 @@
 #ifndef BUILD_NUMBER_H_ 
 #define BUILD_NUMBER_H_ 
 
-#define BUILDNUMBER 7391
-#define BUILDNUMBER_STR "7391" 
+#define BUILDNUMBER 7392
+#define BUILDNUMBER_STR "7392" 
 
 #endif /* BUILD_NUMBER_H_ */
