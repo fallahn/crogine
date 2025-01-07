d35eaf1aaf65b036e4b0d2d61d3684d822734b18
fallahn <matty_styles@hotmail.com>
2025-01-07 13:06:24 +0000

    assert websocket test

--- samples/golf/buildnumber.h
+++ samples/golf/buildnumber.h
@@ -3,7 +3,7 @@
 #ifndef BUILD_NUMBER_H_ 
 #define BUILD_NUMBER_H_ 
 
-#define BUILDNUMBER 7392
-#define BUILDNUMBER_STR "7392" 
+#define BUILDNUMBER 7393
+#define BUILDNUMBER_STR "7393" 
 
 #endif /* BUILD_NUMBER_H_ */
