22a428396677b8c8f6b2bee720c6e0e012b2bf93
fallahn <matty_styles@hotmail.com>
2025-02-25 13:42:04 +0000

    add server side bucket collision

--- samples/golf/src/golf/CoinSystem.cpp
+++ samples/golf/src/golf/CoinSystem.cpp
@@ -35,6 +35,37 @@ source distribution.
 #include <crogine/ecs/components/Transform.hpp>
 #include <crogine/detail/glm/gtx/norm.hpp>
 
+namespace
+{
+    constexpr glm::vec2 BucketCentre = glm::vec2(20.f, 23.f);
+    constexpr float BucketRadius = 32.f; //crude broad phase
+    constexpr float BucketRadSqr = BucketRadius * BucketRadius;
+    constexpr cro::FloatRect BucketLeft = {1.f, 0.f, 6.f, 46.f};
+    constexpr cro::FloatRect BucketRight = {35.f, 0.f, 6.f, 46.f};
+    constexpr cro::FloatRect CoinRect = {-2.f, -2.f, 4.f, 4.f};
+
+    constexpr float Dampening = 0.05f;
+
+    glm::vec3 getManifold(const cro::FloatRect& overlap, const glm::vec2 collisionNormal)
+    {
+        //the collision normal is stored in x and y, with the penetration in z
+        glm::vec3 manifold = { 0.f, 1.f, 0.f };
+
+        if (overlap.width < overlap.height)
+        {
+            manifold.x = (collisionNormal.x < 0) ? 1.f : -1.f;
+            manifold.z = overlap.width;
+        }
+        else
+        {
+            manifold.y = (collisionNormal.y < 0) ? 1.f : -1.f;
+            manifold.z = overlap.height;
+        }
+        //LogI << manifold << std::endl;
+        return manifold;
+    }
+}
+
 CoinSystem::CoinSystem(cro::MessageBus& mb, sv::SharedData& sd)
     : cro::System(mb, typeid(CoinSystem)),
     m_sharedData(sd)
@@ -50,16 +81,70 @@ void CoinSystem::process(float dt)
 {
     if (m_bucketEnt.isValid())
     {
+        const auto bucketLeft = BucketLeft.transform(m_bucketEnt.getComponent<cro::Transform>().getLocalTransform());
+        const auto bucketRight = BucketRight.transform(m_bucketEnt.getComponent<cro::Transform>().getLocalTransform());
+
+        const auto leftCentre = glm::vec2(bucketLeft.left + (bucketLeft.width / 2.f), bucketLeft.bottom + (bucketLeft.height / 2.f));
+        const auto rightCentre = glm::vec2(bucketRight.left + (bucketRight.width / 2.f), bucketRight.bottom + (bucketRight.height / 2.f));
+
+        const auto canCentre = glm::vec2(m_bucketEnt.getComponent<cro::Transform>().getPosition()) + BucketCentre;
+
         auto& entities = getEntities();
         for (auto entity : entities)
         {
             auto& coin = entity.getComponent<Coin>();
             auto& tx = entity.getComponent<cro::Transform>();
 
-            tx.move(coin.velocity * dt);
-            coin.velocity += Coin::Gravity * dt;
+            glm::vec2 startPos = tx.getPosition();
+            if (glm::length2(startPos - canCentre) < BucketRadSqr)
+            {
+                static constexpr std::int32_t StepCount = 4; //TODO we could try adjusting this based on velocity magnitude
+                const auto step = (coin.velocity * dt) / static_cast<float>(StepCount);
 
-            //TODO collision with can
+                //collision with can
+                for (auto i = 0; i < StepCount; ++i)
+                {
+                    const auto pos = glm::vec2(tx.getPosition()) + (step * static_cast<float>(i));
+
+                    auto coinRect = CoinRect;
+                    coinRect.left += pos.x;
+                    coinRect.bottom += pos.y;
+
+                    cro::FloatRect overlap;
+
+                    auto testCollision = [&](const cro::FloatRect& target)
+                        {
+                            if (coinRect.intersects(target, overlap))
+                            {
+                                const auto normal = leftCentre - pos;
+                                const auto manifold = getManifold(overlap, normal);
+
+                                tx.setPosition(pos + (glm::vec2(manifold) * (manifold.z * 2.f)));
+                                coin.velocity = glm::reflect(coin.velocity, glm::vec2(manifold));
+
+                                coin.velocity *= Dampening;
+                                return true;
+                            }
+                            return false;
+                        };
+
+                    if (testCollision(bucketLeft) || testCollision(bucketRight))
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        tx.move(step);
+                    }
+                }
+            }
+            else
+            {
+                //we're far enough to just move
+                tx.move(coin.velocity * dt);
+            }
+
+            coin.velocity += Coin::Gravity * dt;
 
             if (tx.getPosition().y < 0)
             {
