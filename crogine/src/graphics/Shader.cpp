028720588d03b924707a8cc5386e3a485baabc76
fallahn <matty_styles@hotmail.com>
2025-03-24 20:15:29 +0000

    revert the reversion

--- crogine/src/graphics/Shader.cpp
+++ crogine/src/graphics/Shader.cpp
@@ -267,11 +267,11 @@ bool Shader::loadFromSource(const char* vertex, const char* geometry, const char
     std::string version = "#version 100\n#define MOBILE\n" + vendorDef;
     const char* src[] = { version.c_str(), precision.c_str(), defines, vertex};
 #else
-//#if defined GL41 || defined __APPLE__
+#if defined GL41 || defined __APPLE__
     std::string version = "#version 410 core\n" + vendorDef;
-//#else
-//    std::string version = "#version 460 core\n" + vendorDef;
-//#endif // GL41
+#else
+    std::string version = "#version 460 core\n" + vendorDef;
+#endif // GL41
     const char* src[] = { version.c_str(), precision.c_str(), defines, vertex};
 #endif //__ANDROID__
 
